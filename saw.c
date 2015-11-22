7d57c6fb6b2f4aedaedfa08371a74c355397251f
ryanfmurphy <ryanfmurphy@gmail.com>
2015-11-22 11:40:47 -0600

    saw - this version stays the same volume, or maybe gets a bit louder
    changing multiplication while keeping mod constant

--- saw.c
+++ saw.c
@@ -4,12 +4,12 @@
 // ALSO COOL: ./1 | speakerpipe -r "44.1 kHz" (sounds weird and rhythmic)
 
 int main(){
-    #define NUM_MODS 5
-    int mods[NUM_MODS] = {256,128,64,32,16};
-    for (int j=0;j<NUM_MODS;j++) {
+    #define NUM_MULTS 5
+    int mults[NUM_MULTS] = {1,2,4,8,16};
+    for (int j=0;j<NUM_MULTS;j++) {
         for (int t=0;t<256*100;t++) {
             putchar(
-                t % mods[j]
+                t*mults[j] % 256
             );
         }
     }
